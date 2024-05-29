9c3238db779eafcd3828cf06d22f8b43a9299203
satyamx <satyamsingh748846@gmail.com>
2024-05-30 02:11:40 +0530

    lastest

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ import { logger } from "./config/logger.config.js";
 import { socketHandlers } from "./config/socket.config.js";
 
 const app = express();
+app.set('trust proxy', true);
+
 const server = createServer(app);
 export const io = new Server(server, {
   cors: {
