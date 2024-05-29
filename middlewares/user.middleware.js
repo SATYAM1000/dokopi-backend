507d95b28809ea38ee02ec1ec3d4325ad782aaf1
satyamx <satyamsingh748846@gmail.com>
2024-05-30 04:03:59 +0530

    changed env values

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -6,7 +6,7 @@ const AUTH_ERROR_MESSAGE = "Please authenticate using a valid token!";
 export const verifyUser = async (req, res, next) => {
   try {
     const token = req.header("Authorization");
-    console.log("visited-----> 1")
+    console.log("visited-----> 1999999999")
     if (!token) {
       return res.status(401).json({
         msg: AUTH_ERROR_MESSAGE,
--- utils/decode-jwt.js
+++ utils/decode-jwt.js
@@ -2,22 +2,18 @@ import { logger } from "../config/logger.config.js";
 import { decode } from "next-auth/jwt";
 
 export const decodeJWT = async (token) => {
-  const SALT_KEY =
-    process.env.NODE_ENV === "production"
-      ? process.env.JWT_SALT
-      : process.env.JWT_SALT_DEV;
+  console.log("token is ", token);
+  console.log("env is ", process.env.NODE_ENV);
+  const SALT_KEY = process.env.NODE_ENV === 'production' ? "__Secure-authjs.session-token" : "authjs.session-token";
+  console.log("SALT_KEY is ", SALT_KEY);
+
 
   try {
-    if(process.env.NODE_ENV === "production"){
-      return await decode({ token: token, salt: process.env.JWT_SALT, secret: process.env.JWT_SECRET });
-    }
-
-    const decodedToken = await decode({ token: token, salt: process.env.JWT_SALT_DEV, secret: process.env.JWT_SECRET });
-    // const decodedToken = await decode({
-    //   token: token,
-    //   salt: SALT_KEY,
-    //   secret: process.env.JWT_SECRET,
-    // });
+    const decodedToken = await decode({
+      token: token,
+      salt: SALT_KEY,
+      secret: process.env.JWT_SECRET,
+    });
 
     return decodedToken;
   } catch (error) {
