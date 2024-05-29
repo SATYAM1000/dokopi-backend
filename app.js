7907692a9eb1698396007145f71c47abc8f8908c
satyamx <satyamsingh748846@gmail.com>
2024-05-30 02:08:07 +0530

    lastest

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", limiter);
+app.use(limiter);
 app.use(helmet());
 app.use(hpp());
 app.use(ExpressMongoSanitize());
