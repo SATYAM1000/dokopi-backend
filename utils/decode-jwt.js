b81709b1abfd314bc02873edd84f39f71f357cde
satyamx <satyamsingh748846@gmail.com>
2024-05-30 03:09:05 +0530

    changed env values

--- utils/decode-jwt.js
+++ utils/decode-jwt.js
@@ -2,10 +2,14 @@ import { logger } from "../config/logger.config.js";
 import { decode } from "next-auth/jwt";
 
 export const decodeJWT = async (token) => {
+  const SALT_KEY =
+    process.env.NODE_ENV === "development"
+      ? process.env.JWT_SALT_DEV
+      : process.env.JWT_SALT;
   try {
     const decodedToken = await decode({
       token: token,
-      salt: process.env.JWT_SALT,
+      salt: SALT_KEY,
       secret: process.env.JWT_SECRET,
     });
 
