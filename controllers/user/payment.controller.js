0ff1a1366482f4437d47c37e65df07a9564d50da
satyamx <satyamsingh748846@gmail.com>
2024-05-30 19:20:23 +0530

    payment api done

--- controllers/user/payment.controller.js
+++ controllers/user/payment.controller.js
@@ -1,5 +1,6 @@
 import { instance } from "../../config/razorpay-instance.config.js";
 import crypto from "crypto";
+import { logger } from "../../config/logger.config.js";
 export const checkout = async (req, res) => {
   const options = {
     amount: Number(req.body.amount * 100), // amount in the smallest currency unit
@@ -18,7 +19,6 @@ export const checkout = async (req, res) => {
     res.status(500).json({
       success: false,
       message: error.message,
-
     });
   }
 };
@@ -35,25 +35,39 @@ export const paymentVerification = async (req, res) => {
       .update(body.toString())
       .digest("hex");
 
-      console.log("signature is ", expectedSignature);
-      console.log("razorpay signature is ", razorpay_signature);
+    console.log("signature is ", expectedSignature);
+    console.log("razorpay signature is ", razorpay_signature);
 
-      const isAuthentic = expectedSignature === razorpay_signature;
+    const isAuthentic = expectedSignature === razorpay_signature;
 
-      if (isAuthentic) {
-        // Database comes here
-        res.redirect(
-          `http://localhost:3000/payment/success?reference=${razorpay_payment_id}`
-        );
-      } else {
-        res.status(400).json({
-          success: false,
-        });
-      }
-
-   
+    if (isAuthentic) {
+      // Database comes here
+      res.redirect(
+        `http://localhost:3000/payment/success?reference=${razorpay_payment_id}`
+      );
+    } else {
+      res.status(400).json({
+        success: false,
+      });
+    }
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+export const getRazorpayKey = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      key: process.env.RAZORPAY_KEY_ID,
+    });
+  } catch (error) {
+    logger.error("Error while getting Razorpay key: ", error.message);
     res.status(500).json({
       success: false,
       message: error.message,
--- routes/user/payment.route.js
+++ routes/user/payment.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { checkout, paymentVerification } from "../../controllers/user/payment.controller.js";
+import {
+  checkout,
+  getRazorpayKey,
+  paymentVerification,
+} from "../../controllers/user/payment.controller.js";
+import { verifyUser } from "../../middlewares/user.middleware.js";
 
 const paymentRouter = express.Router();
 
-paymentRouter.post("/user-checkout",checkout);
-paymentRouter.post("/verify",paymentVerification);
-
+paymentRouter.post("/user-checkout", verifyUser, checkout);
+paymentRouter.post("/verify", verifyUser, paymentVerification);
+paymentRouter.get("/razorpay-key", verifyUser, getRazorpayKey);
 export default paymentRouter;
