bbce1a04990b1eb84b98f0bc7f0ceca26f6c5141
satyamx <satyamsingh748846@gmail.com>
2024-06-15 20:26:35 +0530

    socket implementation

--- config/socket.config.js
+++ config/socket.config.js
@@ -1,43 +1,29 @@
-import { XeroxStore } from "../models/store.model.js";
 import { User } from "../models/user.model.js";
 
 export const socketHandlers = (io, logger) => {
   io.on("connection", (socket) => {
     logger.info("New client connected! ", socket.id);
 
-    socket.on(`userLogin`, async (userId) => {
+    socket.on("userConnect", async ({ userId }) => {
       try {
-        const userSocket = await User.findOneAndUpdate(
-          {
-            _id: userId,
-          },
-          {
-            socketId: socket.id,
-          },
-          {
-            upsert: true,
-            new: true,
-          }
+        const user = await User.findOneAndUpdate(
+          { _id: userId },
+          { socketId: socket.id },
+          { new: true }
         );
-
-        logger.info(`User logged in with ID ${userId}`);
+        logger.info(`User ${userId} connected and socket ID ${socket.id} stored`);
       } catch (error) {
-        logger.error("Error handling user login:", error);
+        logger.error("Error handling user connection:", error);
       }
     });
 
-
-    socket.on("disconnect", async() => {
+    socket.on("disconnect", async () => {
       logger.info("Client disconnected!", socket.id);
       await User.findOneAndUpdate(
-        {
-          socketId: socket.id,
-        },
-        {
-          socketId: "",
-        }
-      )
-    })
+        { socketId: socket.id },
+        { socketId: "" }
+      );
+    });
   });
 };
 
